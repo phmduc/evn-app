099d0dc7095ac89ccff9fcb6ff628f0b17b0a365
phmduc <phmduc1012@gmail.com>
2023-07-12 14:04:51 +0700

    push route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Switch } from 'react-router';
+import { useState } from 'react';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/home.js';
 import Profile from './pages/profile/profile.js';
 import Contact from './pages/Contact/Contact';
@@ -26,35 +27,50 @@ import Shopping from './pages/shopping/shopping';
 import Vip from './pages/vip/vip';
 import Withdraw from './pages/withdraw/withdraw';
 
-function App() {
+function ProfileRoute() {
+  const isLoggedIn = sessionStorage.getItem('user_id');
+  const [path, setPath] = useState(window.location.pathname);
   return (
-   <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path="/" element={<Profile />} />
+        <Route path="/record/*" element={<Record />} />
+        <Route path='/ourproject/*' element={< OurProject/>} />
+        <Route path='/paymentmethod/*' element={< PaymentMethod/>} />
+        <Route path='/PersonalSetting/*' element={< PersonalSetting/>} />
+        <Route path='/Qrcode/*' element={< Qrcode/>} />
+        <Route path='/RealNameAuthen/*' element={< RealNameAuthen/>} />
+        <Route path='/Recharge/*' element={< Recharge/>} />
+        <Route path='/Record/*' element={< Record/>} />
+        <Route path='/Shopping/*' element={< Shopping/>} />
+        <Route path='/Vip/*' element={< Vip/>} />
+        <Route path='/Withdraw/*' element={< Withdraw/>} />
+        <Route path='/accountdetail/*' element={<AccountDetail />} />
+        <Route path='/changeauthen/*' element={<Changeauthen />} />
+        <Route path='/dailycheck/*' element={<DailyCheck />} />
+      </Routes>
+  );
+}
+
+function App() {
+
+  const isLoggedIn = sessionStorage.getItem('user_id');
+  const [path, setPath] = useState(window.location.pathname);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path='/*' element={<Home />} />
+        <Route path='/profile/*' element={isLoggedIn ? <ProfileRoute /> : <Authen/>} />
         <Route path='/category' element={<Category />} />
         <Route path='/aboutus' element={<AboutUs />} />
         <Route path='/contact' element={<Contact />} />
-        <Route path='/accountdetail' element={<AccountDetail />} />
         <Route path='/authen' element={<Authen />} />
         <Route path='/blogs' element={<Blogs />} />
         <Route path='/blogdetail' element={<Blogdetail />} />
-        <Route path='/changeauthen' element={<Changeauthen />} />
-        <Route path='/dailycheck' element={<DailyCheck />} />
         <Route path='/notification' element={<Notification />} />
         <Route path='/notifydetail' element={<NotifyDetail />} />
-        <Route path='/ourproject' element={< OurProject/>} />
-        <Route path='/paymentmethod' element={< PaymentMethod/>} />
-        <Route path='/PersonalSetting' element={< PersonalSetting/>} />
-        <Route path='/Qrcode' element={< Qrcode/>} />
-        <Route path='/RealNameAuthen' element={< RealNameAuthen/>} />
-        <Route path='/Recharge' element={< Recharge/>} />
-        <Route path='/Record' element={< Record/>} />
-        <Route path='/Shopping' element={< Shopping/>} />
-        <Route path='/Vip' element={< Vip/>} />
-        <Route path='/Withdraw' element={< Withdraw/>} />
       </Routes>
-   </BrowserRouter>
+    </Router>
   );
 }
 
